2d775e5153eeab2f38b905b3cc03e85616015e40
Prashant <imprashantsingh94@gmail.com>
2021-09-04 00:56:30 +0530

    First commit

---
+++ app/components/FourZeroFour.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class FourZeroFour extends React.Component {
+ render(){
+     return (
+        <h2>404 NOT FOUND!</h2>
+     )
+ }
+}
---
+++ app/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import Popular from "./components/Popular"
+import Battle from "./components/Battle"
+import {ThemeProvider} from "./contexts/theme"
+import Nav from "./components/Nav"
+import FourZeroFour from "./components/FourZeroFour"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import Results from "./components/Results"
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({theme}) => ({
+         theme: theme === "light" ? "dark" : "light"
+        }))
+      }
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Switch>
+              <Route exact path='/' component={Popular} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/battle/results' component={Results} />
+              <Route component={FourZeroFour} />
+              </Switch>
+
+
+
+            </div>
+          </div>
+
+        </ThemeProvider>
+      </Router>
+
+
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"))
